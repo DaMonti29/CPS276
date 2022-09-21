7db488b4f5a5024f430ad30939e005ae40e11f12
damonti <dan_monti_29@yahoo.com>
2022-09-21 10:35:07 -0400

    "updates"

--- phpmysqltest/index.php
+++ phpmysqltest/index.php
@@ -1,9 +1,9 @@
+
 <?php
 require_once 'classes/Crud.php';
 $crud = new Crud();
 $output = $crud->getCustomers();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,9 +15,20 @@ $output = $crud->getCustomers();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>MySQL PHP Test</title>
 </head>
-<body class="container">
+<body>
+<!doctype html>
+<html lang="en">
+  <<body class="container">
     <?php  
         echo $output; 
     ?>
+
+  <body>
+    
+    </div>
+  </body>
+</html>
+
+
 </body>
 </html>
