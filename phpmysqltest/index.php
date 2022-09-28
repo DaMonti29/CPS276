890e004b2bc28157abdf8fdd2156aba8c6895e1c
damonti <dan_monti_29@yahoo.com>
2022-09-28 08:59:35 -0400

    updates

--- phpmysqltest/index.php
+++ phpmysqltest/index.php
@@ -1,27 +1,30 @@
 
-<?php
-require_once 'classes/Crud.php';
-$crud = new Crud();
-$output = $crud->getCustomers();
-?>
+class Phone {
+public $color;
+public $manufacturer;
+static public $numberSold = 13;
+}
+Phone::$numberSold+=5;
+echo Phone::$numberSold;
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta charset = "UTF-8">
+   <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+   <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>MySQL PHP Test</title>
+<title>Document</title>
+
 </head>
 <body>
 <!doctype html>
 <html lang="en">
   <<body class="container">
-    <?php  
-        echo $output; 
-    ?>
+
+  Phone::$numberSold+=5;
+echo Phone::$numberSold;
 
   <body>
     
